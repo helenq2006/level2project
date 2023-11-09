3dbe70107beab33b49c2f58351691459e19405ff
helenq2006 <114586722+helenq2006@users.noreply.github.com>
2023-11-09 09:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const timBurton = "Timothy Walter Burton is an American filmmaker, animator, and artist. Known for pioneering goth culture in the American film industry, Burton is revered for his gothic horror and fantasy films (Source: Wikipedia)";
-const jamieLeeCurtis = "Jamie Lee Curtis is an American actress, producer, and children's author. Known for her performances in the horror and slasher genres, she is regarded as a scream queen, in addition to roles in comedies (Source: Wikipedia)";
-const tobinBell = "Tobin Bell is an American actor most recognized for his role as John Kramer aka Jigsaw in the Saw franchise. He started his acting career in the late 1970s and early 1980s doing stand-ins and background work on feature films (Source: Wikipedia)";
-const willSmith = "(Booth 120) An American actor, rapper, and film producer who recieved multiple accolades, including an Academy award, a BAFTA Awardm and four Grammys. As of 2023, his films have grossed over $9.3 billion globally, making him one of Hollywood's most bankable stars.";
-const sandraBullock = "(Booth 122) An American actress and producer who has been named one of Time's 100 most influential people in the world. She is known for her stunning performances in Ocean's 8, The Blind Side, and The Proposal."
-const vinDiesel = "Vin Diesel- (Booth 134) Mark Sinclair aka Vin Diesel, is an American actor and film producer. One of the world's highest grossing actors, he is best known for playing Dominic Toretto in the Fast & Furious franchise."
-const tomHiddleston = "(Booth 162) An English actor who has quickly earned a place in fan's hearts with his role as Loki in the Marvel Cinematic Universe.";
-
-function showInfo(guest) {
-    show = document.getElementById(guest);
-    show.innerHTML = guest;
+function setVisibility(id, visibility) {
+    document.getElementById(id).style.display = visibility;
 }
